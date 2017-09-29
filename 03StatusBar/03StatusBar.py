10dc726ce13ea23456dc8c995d0614dc342724fd
kayukawa-seita <k940805k.lab@gmail.com>
2017-09-29 23:33:58 +0900

    add 03

---
+++ 03StatusBar/03StatusBar.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+import sys
+from PyQt5.QtWidgets import QMainWindow, QPushButton, QApplication, QToolTip
+from PyQt5.QtWidgets import QMainWindow, QPushButton, QApplication, QToolTip
+from PyQt5.QtCore import QCoreApplication
+from PyQt5.QtGui import QFont
