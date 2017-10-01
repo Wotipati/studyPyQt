ec57ccc77d79105989df8799cf2c09bef4b66d4e
Wotipati <k940805k.lab@gmail.com>
2017-10-01 13:10:58 +0900

    update

--- 04MenuBar&ToolBar/04MenuBar&ToolBar.py
+++ 04MenuBar&ToolBar/04MenuBar&ToolBar.py
@@ -13,13 +13,13 @@ class Bars(QMainWindow):
     def init_ui(self):
 
         exit_gui = QApplication.style().standardIcon(QStyle.SP_TitleBarCloseButton)
-        exit_action = QAction(exit_gui, 'Exit', self)
+        exit_action = QAction(exit_gui, '&Exit', self)
         exit_action.setShortcut('Ctrl+Q')
         exit_action.setStatusTip('Exit Application')
         exit_action.triggered.connect(QCoreApplication.instance().quit)
 
         qt_info_gui = QApplication.style().standardIcon(QStyle.SP_TitleBarMenuButton)
-        qt_info_action = QAction(qt_info_gui, 'about Qt', self)
+        qt_info_action = QAction(qt_info_gui, '&about Qt', self)
         qt_info_action.setShortcut('ctrl+I')
         qt_info_action.setStatusTip('show Qt info')
         qt_info_action.triggered.connect(QCoreApplication.instance().quit)
@@ -27,13 +27,15 @@ class Bars(QMainWindow):
         self.statusBar()
 
         menubar = self.menuBar()
-        file_menu = menubar.addMenu('File')
+        file_menu = menubar.addMenu('&File')
         file_menu.addAction(exit_action)
         file_menu.addAction(qt_info_action)
 
-        toolbar = self.addToolBar('Exit')
+        toolbar = self.addToolBar('&toolbar')
         toolbar.addAction(exit_action)
-        toolbar.addAction(qt_info_action)
+
+        toolbar2 = self.addToolBar('&tooooolbar')
+        toolbar2.addAction(qt_info_action)
 
         self.setGeometry(200, 300, 400, 300)
         self.setWindowTitle('Menu and Tool Bars')
--- 04MenuBar&ToolBar/README.md
+++ 04MenuBar&ToolBar/README.md
@@ -18,4 +18,48 @@
     - ステータスバーに表示するTipsを設定
     - カーソルが上に来た時にTipsがステータスバーに表示される
 
+  - ``.triggered.connect(`SLOT`)``
+    - クリックされた時に発生するシグナル
+
+  - ``.toggled.connect(`SLOT`)``
+    - トグルされた時に発生するシグナル
+
+
 ##### MenuBarへの配置
+```python:sample
+  # アイコンの設定
+  exit_gui = QApplication.style().standardIcon(QStyle.SP_TitleBarCloseButton)
+  # QActionオブジェクトの作成
+  exit_action = QAction(exit_gui, '&Exit', self)
+  # シグナルとスロットの連携
+  exit_action.triggered.connect(QCoreApplication.instance().quit)
+
+  # メニューバーの作成
+  menubar = self.menuBar()
+  # メニューバーに"File"という名前のメニューを追加
+  file_menu = menubar.addMenu('&File')
+  # "File"メニューに上で設定したQActionオブジェクト(exit_action)を追加
+  file_menu.addAction(exit_action)
+```
+
+
+##### ToolBarへの配置
+```python:sample
+  exit_gui = QApplication.style().standardIcon(QStyle.SP_TitleBarCloseButton)
+  exit_action = QAction(exit_gui, '&Exit', self)
+  exit_action.triggered.connect(QCoreApplication.instance().quit)
+
+  qt_info_gui = QApplication.style().standardIcon(QStyle.SP_TitleBarMenuButton)
+  qt_info_action = QAction(qt_info_gui, '&about Qt', self)
+  qt_info_action.triggered.connect(QCoreApplication.instance().quit)
+
+  # ツールバーの作成
+  toolbar = self.addToolBar('&toolbar')
+  # ツールバーに上で設定したQActionオブジェクト(exit_action)を追加
+  toolbar.addAction(exit_action)
+
+  # 別のツールバーの作成
+  toolbar2 = self.addToolBar('&tooooolbar')
+  # ツールバーに上で設定したQActionオブジェクト(qt_info_action)を追加
+  toolbar2.addAction(qt_info_action)
+```
