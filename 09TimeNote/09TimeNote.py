7a1b94dd57860fb6237accceeefa7ffc27872401
Wotipati <k940805k.lab@gmail.com>
2017-10-23 22:55:33 +0900

    add grid

--- 09TimeNote/09TimeNote.py
+++ 09TimeNote/09TimeNote.py
@@ -2,8 +2,8 @@
 
 import sys
 
-from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QPushButton, QAction, QVBoxLayout, QHBoxLayout,\
-                            QLCDNumber, QTextEdit
+from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QPushButton, QGridLayout, QVBoxLayout, QHBoxLayout,\
+                            QLCDNumber, QTextEdit, QAction, QLabel
 from PyQt5.QtCore import QCoreApplication, QTimer
 from PyQt5.QtGui import QIcon
 from stopwatch import Stopwatch
@@ -17,15 +17,32 @@ class Timenote(QMainWindow):
         self.main_widget = QWidget(self)
         self.setCentralWidget(self.main_widget)
 
+        self.label_now_subject = QLabel
         self.layout_main = QHBoxLayout()
         self.layout_user = QVBoxLayout()
-
+        self.layout_grid = QGridLayout()
+        self.buttons_subject = []
         self.init_ui()
 
     def init_ui(self):
+        self.layout_user.addWidget(self.label_now_subject)
+
         stopwatch = Stopwatch()
         self.layout_user.addLayout(stopwatch.layout_main)
         self.main_widget.setLayout(self.layout_user)
+
+        labels_subject= ['Meeting', 'Implementation', 'Experiment',
+                        'Research', 'Paper survey', 'Writing a paper',
+                        'Making slides', 'Taking a class', 'Chores']
+
+        positions_subject = [(i,j) for i in range(3) for j in range(3)]
+
+        for position, subject in zip(positions_subject, labels_subject):
+            button = QPushButton(subject)
+            self.buttons_subject.append(button)
+            self.layout_grid.addWidget(button, *position)
+
+        self.layout_user.addLayout(self.layout_grid)
 
         exit_action = QAction(QIcon('./icon/exit.png'), '&Exit', self)
         exit_action.setShortcut('Ctrl+Q')
